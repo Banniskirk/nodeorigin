78577de9aaaf7c643e8886f6603d46427b8eb10a
keg <keg@mdacru.com>
2020-06-12 09:06:27 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+// Init Middleware
+
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+app.get('/talk', (req, res) => {
+  res.send('Bla bla bla');
+});
+
+// Define Routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
