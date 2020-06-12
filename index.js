b8c42d1136c6413439a2764594b1e5edf35859ad
keg <keg@mdacru.com>
2020-06-12 09:53:09 -0400

    port 5000

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
